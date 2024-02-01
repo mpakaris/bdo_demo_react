a32e7b92da05bbfba69e684f1237edc3cd1b1050
Nikos Mpakaris <57401398+mpakaris@users.noreply.github.com>
2024-02-01 08:55:08 +0100

    implemented Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import "./App.css";
 
+// style={{ border: "3px solid red" }}
+
 function App() {
   return (
     <div className="App">
-      <p className="fs-1 text-decoration-underline">BDO digital</p>
+      <div className="hero-section">
+        <div className="hero-overlay"></div>
+        <div className="hero-content">
+          <h1>Welcome to BDO digital</h1>
+          <p>This is a Demo Task Application</p>
+          <p>by Nikos Mpakaris</p>
+          <div className="btn-selection">
+            <button className="btn btn-primary">User Management</button>
+            <button className="btn btn-primary ms-3">Task Management</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
