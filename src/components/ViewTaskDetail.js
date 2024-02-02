432f66fe3d800ab534e3cf648857b2fa9d2cf462
Nikos Mpakaris <57401398+mpakaris@users.noreply.github.com>
2024-02-02 23:33:16 +0100

    removed unneeded code

--- src/components/ViewTaskDetail.js
+++ src/components/ViewTaskDetail.js
@@ -8,13 +8,10 @@ function ViewTaskDetails({ showViewModal, handleViewModal, activeTask }) {
         <Modal.Title>View Task Details</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <h4>Title | {activeTask ? activeTask.taskTitle || "" : ""}</h4>
-        <h5>
-          Description | {activeTask ? activeTask.taskDescription || "" : ""}
-        </h5>
+        <h4>Title | {activeTask ? activeTask.taskTitle : ""}</h4>
+        <h5>Description | {activeTask ? activeTask.taskDescription : ""}</h5>
         <h6>
-          Assignee |{" "}
-          {activeTask && activeTask.user ? activeTask.user.name || "" : ""}
+          Assignee | {activeTask && activeTask.user ? activeTask.user.name : ""}
         </h6>
       </Modal.Body>
       <Modal.Footer>
