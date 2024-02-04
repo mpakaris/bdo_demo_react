09db59008ef7af35a5b83bd7d2b358a26338c773
Nikos Mpakaris <57401398+mpakaris@users.noreply.github.com>
2024-02-04 07:58:31 +0100

    style check

--- src/components/MyToastError.js
+++ src/components/MyToastError.js
@@ -3,12 +3,14 @@ import { Toast } from "react-bootstrap";
 
 function MyToastError({ show, onClose, children }) {
   return (
-    <Toast show={show} onClose={onClose} delay={3000} autohide>
-      <Toast.Header>
-        <strong className="mr-auto">Error</strong>
-      </Toast.Header>
-      <Toast.Body className="text-warning">{children}</Toast.Body>
-    </Toast>
+    <div className="toast-container">
+      <Toast show={show} onClose={onClose} delay={3000} autohide>
+        <Toast.Header>
+          <strong className="mr-auto">Error</strong>
+        </Toast.Header>
+        <Toast.Body className="text-warning">{children}</Toast.Body>
+      </Toast>
+    </div>
   );
 }
 
